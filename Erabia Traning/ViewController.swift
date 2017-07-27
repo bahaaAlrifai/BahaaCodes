ef4be04f5f0f8f0235da9019a58ff58a09c28209
bahaa alrifai <bahaa@bahaas-macbook-pro.local>
2017-07-27 08:32:40 +0300

    Initial Commit

---
+++ Erabia Traning/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Erabia Traning
+//
+//  Created by bahaa alrifai on 7/27/17.
+//  Copyright © 2017 bahaa alrifai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
